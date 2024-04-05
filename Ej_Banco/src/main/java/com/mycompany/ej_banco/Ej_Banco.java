0432a063bb51139fbd04d8e7f51202b4b0296054
Joseph-Mamian <jsmamianb@ufpso.edu.co>
2024-04-05 10:04:50 -0500

    bank exercise

--- Ej_Banco/src/main/java/com/mycompany/ej_banco/Ej_Banco.java
+++ Ej_Banco/src/main/java/com/mycompany/ej_banco/Ej_Banco.java
@@ -1,8 +1,25 @@
 package com.mycompany.ej_banco;
+import com.mycompany.ej_banco.people.Holder;
 
 public class Ej_Banco {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Holder people1 = new Holder ("pepito");
+        Holder people2 = new Holder ("Chucho", 500_000);
+        
+        people1.getName();
+        people1.getMoney();
+        people2.getName();
+        people2.getMoney();
+        
+        System.out.println("El saldo de " + people1.getName() + " es de " + people1.getMoney());
+        System.out.println("El saldo de " + people2.getName() + " es de " + people2.getMoney());
+        
+        people1.consigMoney(100_000);
+        people2.consigMoney(100_000);
+        
+        people1.withdrawmoney(200);
+        people2.withdrawmoney(-1);
+        
     }
 }
--- Ej_Banco/src/main/java/com/mycompany/ej_banco/people/Holder.java
+++ Ej_Banco/src/main/java/com/mycompany/ej_banco/people/Holder.java
@@ -14,5 +14,32 @@ public class Holder {
         this.money = 0;
     }
 
-        
+    public String getName() {
+        return name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+    
+   public void consigMoney (double qty){
+   if (qty <= 0) {
+       System.out.println("No se puede consignar"); 
+     }
+     this.money +=qty;
+       System.out.println("Consignacion realizada");
+   }
+    
+   public void withdrawmoney (double qty) {
+   if (qty > this.money){
+           System.out.println("Saldo insuficiente");
+   return;
+   }
+   this.money -= qty;
+       System.out.println("Retiro realizado");           
+   }
 }
